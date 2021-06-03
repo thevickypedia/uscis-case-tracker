d7f5b44a90a22f0ba13d580b7a7004a959bb1113
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-06-03 17:44:00 -0500

    use sessions from requests module

--- tracker.py
+++ tracker.py
@@ -1,13 +1,25 @@
 from os import environ
 
 from bs4 import BeautifulSoup
-from requests import get
+from requests import Session
 
 url = f'https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum={environ.get("RECEIPT")}'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/58.0.3029.110 Safari/537.3'
+}
+with Session() as session:
+    session.headers = headers
+    response = session.get(url=url, headers=headers)
 
-response = get(url=url)
+    # to use lxml (instead of html.parser) run pip install lxml before hand
+    scrapped = BeautifulSoup(response.text, "html.parser")
+    soup = scrapped.find_all('div', {'class': 'rows text-center'})[0]
 
-scrapped = BeautifulSoup(response.text, "html.parser")
-soup = scrapped.find_all('div', {'class': 'rows text-center'})[0]
-print(soup.find('h1').text)
-print(soup.find('p').text)
+    title = soup.find('h1').text
+    description = soup.find('p').text
+    if title.strip() != 'Case Was Received':
+        print(f'New Update::{title}')
+        print(f'Description::{description}')
+    else:
+        print('No change in Case Status')
